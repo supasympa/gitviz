2f7f5d7c0eb9974f2dea541effa8aa32aaf90beb
Lewis Barclay <lewis.barclay@googlemail.com>
2019-02-19 21:19:53 +0000

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Git-Viz Changes
+
+## Aim
+
+To visualise changes across a code base:
+
+1. Install the component in a git directory: npm i gitviz 
+2. Run the cli command: gitviz changes
+3. Open the folder ./gitviz/index.html
+
+## Steps
+
+### 1. Script the git changes command
+
+### 2. Create a module that wraps the git changes command
+
+### 3. Transform the data from the git changes command into a json object that can be used by a visualiaztion
+
+### 4. Save the data from the git changes command
+
+### 5. Consume the JSON and visualise it with a chart lib
+
+### 6. Add an interface to interrogate the data using React / Hooks
+
+
+## References
+
+* https://stackoverflow.com/questions/1828874/generating-statistics-from-git-repository
+* https://blog.riff.org/2015_10_30_git_tip_of_the_day_show_the_hottest_files_in_a_repo
+
+
---
+++ src/index.js
@@ -0,0 +1,32 @@
+const gitlog = require('gitlog');
+const {resolve} = require('path');
+
+const changesToFiles = (opts = { repoPath: __dirname }) => {
+    const commitMap = gitlog({
+        repo: opts.repoPath
+        , number: opts.max || 1999999
+        , execOptions:
+            {
+                maxBuffer: 99999 * 1024
+            }
+    }).map(c => c.files)
+        .reduce( (filesArray, files) => ([...filesArray, ...files]), [])
+        .sort()
+        .reduce((a, i) =>(( typeof a[i] === 'undefined' ? (a[i] = 1) : (a[i] = a[i] + 1) ) && a ), {});
+
+    return Object.keys(commitMap)
+        .map(k => ([k, commitMap[k]]))
+        .sort((a, b) => (b[1] - a[1]))
+        .slice(0, opts.top || 100)
+        .reduce((a, i) => ((a[i[0]] = i[1]) && a), {});
+};
+
+/*
+console.log(changesToFiles({
+    repoPath: resolve(__dirname, '../../react'),
+    top: 10
+    })
+);
+*/
+
+module.exports = changesToFiles;
