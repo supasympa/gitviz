25947bbb76204a28991852c2c2bf09c7b64eb42e
Lewis Barclay <lewis.barclay@googlemail.com>
2019-02-26 22:51:56 +0000

    chore: apply some basic styles

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
-import { GitChangesAnalyser }  from './GitChangesAnalyser';
 import gitChangeLog from './git-changes-react.json';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
+import changeLog from './git-changes-react.json'
+import { Viz } from './viz';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
-          <h1>GitViz</h1>
-        </header>
-        <GitChangesAnalyser changeLog={gitChangeLog}/>
+      <div className="App container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <header>
+              <div className="bd-brand-logos">
+                <div className="bd-brand-item">
+                  <img src="/logos/logo_transparent.png" className="logo"/>
+                </div>
+              </div>
+            </header>
+            <Viz data={changeLog} />     
+          </div> 
+        </div>
       </div>
     );
   }
