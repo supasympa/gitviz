7d06526f3f644afd7ea66a03316c95be5e7621e6
Lewis Barclay <lewis.barclay@gmail.com>
2019-03-26 20:36:05 +0000

    refactor: minor tidy up

--- packages/web/src/viz/Viz.tsx
+++ packages/web/src/viz/Viz.tsx
@@ -11,33 +11,28 @@ export interface VizProps {
 }
 
 export enum VizState {
-    "BRAND_NEW",
-    "LOADED",
-    "FAILED"
+    'BRAND_NEW',
+    'LOADED',
+    'FAILED',
 }
 
 export const Viz: React.FunctionComponent<VizProps> = (props) => {
     const [vizState, setVizState] = useState(VizState.BRAND_NEW);
     const [gitLogData, setGitLogData] = useState(null);
-    const [breadcrumb, setBreadcrumb] = useState(([] as string[]));
+    const [breadcrumb, setBreadcrumb] = useState([] as string[]);
     const [gitStats, setGitStats] = useState({});
     const chart = useRef(null);
 
     const onDirectoryChange = (breadcrumb: any) => {
-        const path = breadcrumb.join('/').replace(/^root\//, ''); 
-        console.log(path)
+        const path = breadcrumb.join('/').replace(/^root\//, '');
         setBreadcrumb(breadcrumb);
-        setGitStats(
-            calculate(
-             filter({ ...(gitLogData as any), ...{} }, path)  
-                )
-        );
+        setGitStats(calculate(filter({ ...(gitLogData as any), ...{} }, path)));
     };
 
     useEffect(() => {
-        switch (vizState){
+        switch (vizState) {
             case VizState.BRAND_NEW:
-                props.gitLogClient.getLog().then((gl: any)=> {
+                props.gitLogClient.getLog().then((gl: any) => {
                     setGitLogData(gl);
                     setVizState(VizState.LOADED);
                 });
@@ -47,20 +42,23 @@ export const Viz: React.FunctionComponent<VizProps> = (props) => {
                 break;
             default:
                 throw new Error('An error occurred loading git log data');
-                return;    
+                return;
         }
     }, [gitLogData]);
 
-    return <React.Fragment >
+    return (
         <div>
             <div id="chartContainer" ref={chart}>
-                <div className="spinner-border" role="status" style={{width: '7rem', height: '7rem'}} >
+                <div
+                    className="spinner-border"
+                    role="status"
+                    style={{ width: '7rem', height: '7rem' }}
+                >
                     <span className="sr-only">Loading...</span>
                 </div>
             </div>
-            <GitStats gitDirectoryStats={gitStats} />            
+            <GitStats gitDirectoryStats={gitStats} />
             <Breadcrumb path={breadcrumb} />
         </div>
-    </React.Fragment>;
+    );
 };
-
